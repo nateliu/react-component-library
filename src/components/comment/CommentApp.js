35ae6b711a4ff165a07389e22d369a0934e69364
Nate Liu <jinliangliu@163.com>
2017-12-03 13:25:05 +0800

    perform commentInput component

--- src/components/comment/CommentApp.js
+++ src/components/comment/CommentApp.js
@@ -4,10 +4,14 @@ import CommentList from './CommentList';
 import './Comment.css';
 
 export default class CommentApp extends React.Component {
+    handleSubmitComment (comment) {
+        console.log(comment)
+    }
+
     render(){
         return (
             <div className = 'comment-area'>
-                <CommentInput />
+                <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
                 <CommentList />
             </div>
         );
--- src/components/comment/CommentInput.js
+++ src/components/comment/CommentInput.js
@@ -1,10 +1,54 @@
 import React from 'react';
 
 export default class CommentInput extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            userName: '',
+            content:  ''
+        }
+    }
+
+    handleUsernameChange (event) {
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    handleContentChange (event) {
+        this.setState({
+            content: event.target.value
+        })
+    }
+
+    handleSubmit () {
+        if (this.props.onSubmit) {
+            const { userName, content } = this.state
+            this.props.onSubmit({userName, content})
+        }
+        this.setState({ content: '' })
+    }
+    
     render(){
         return (
-            <div>
-                CommentInput               
+            <div className='comment-input'>
+                <div className='comment-field'>
+                    <span className='comment-field-name'>Name:</span>
+                    <div className='comment-field-input'>
+                        <input value={this.state.userName} onChange={this.handleUsernameChange.bind(this)} />
+                    </div>
+                </div>
+
+                <div className='comment-field'>
+                    <span className='comment-field-name'>Comments：</span>
+                    <div className='comment-field-input'>
+                        <textarea value={this.state.content} onChange={this.handleContentChange.bind(this)} />
+                    </div>
+                </div>
+
+                <div className='comment-field-button'>
+                    <button onClick={this.handleSubmit.bind(this)}>Publish</button>
+                </div>
             </div>
         );
     }
