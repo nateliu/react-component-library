654ce175ab623ad8633836a1ecd0ce9c717b0a0d
Nate Liu <jinliangliu@163.com>
2017-12-03 14:28:21 +0800

    peform List and CommentApp

--- src/components/comment/CommentApp.js
+++ src/components/comment/CommentApp.js
@@ -4,15 +4,29 @@ import CommentList from './CommentList';
 import './Comment.css';
 
 export default class CommentApp extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            comments: []
+        }
+    }
+
     handleSubmitComment (comment) {
-        console.log(comment)
+        if (!comment) return;
+        if (!comment.userName) return alert('Name is required!');
+        if (!comment.content) return alert('Content is required!');
+
+        this.state.comments.push(comment);
+        this.setState({
+            comments:this.state.comments
+        })
     }
 
     render(){
         return (
             <div className = 'comment-area'>
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList />
+                <CommentList comments={this.state.comments}/>
             </div>
         );
     }
--- src/components/comment/CommentList.js
+++ src/components/comment/CommentList.js
@@ -3,17 +3,15 @@ import Comment from './Comment';
 
 export default class CommentList extends React.Component {
     render(){
-        const comments = [
-            {userName: 'Jerry', content: 'Hello'},
-            {userName: 'Tomy', content: 'World'},
-            {userName: 'Lucy', content: 'Good'}
-        ]
-
         return (
             <div>
-                {comments.map((comment,i)=><Comment comment={comment} key={i}/> )}            
+                { this.props.comments.map((comment,i)=><Comment comment={comment} key={i}/> )}            
             </div>
         );
     }
 
+}
+
+CommentList.defaultProps = {
+    comments: []
 }
