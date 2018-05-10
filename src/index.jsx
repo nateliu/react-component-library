4f1cea24b75545c6f9c41bb38a35cd87194494e4
Nate Liu <jinliangliu@163.com>
2018-05-10 21:35:28 +0800

    add localized date

--- src/index.jsx
+++ src/index.jsx
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux';
 import CommentApp from './containers/comment/CommentApp';
 import ProfileFinderContainer from './containers/github/ProfileFinderContainer';
 import TrafficApp from './containers/traffic/TrafficApp';
+import LocalFormatedDate from './containers/LocalFormatedDate';
 import store from './store';
 
 ReactDOM.render(
     <Provider store={store}>
         <div>
+            <LocalFormatedDate />
             <ProfileFinderContainer />
             <CommentApp/>
             <TrafficApp/>
